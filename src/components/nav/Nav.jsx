7177b3e4f354d5d3cdfa60fe37caf4364483d4c4
Rasmus Lind <rasmuslindnyberg@gmail.com>
2022-04-11 11:06:16 +0200

    Update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,6 +5,7 @@ import {BiUserCircle} from 'react-icons/bi'
 import {GiSecretBook} from 'react-icons/gi'
 import {RiServiceLine} from 'react-icons/ri'
 import {AiOutlineMessage} from 'react-icons/ai'
+import {MdWork} from 'react-icons/md'
 import { useState } from 'react'
 
 const Nav = () => {
@@ -15,6 +16,7 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserCircle /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><GiSecretBook /></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><MdWork /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage /></a>
     </nav>
   )
